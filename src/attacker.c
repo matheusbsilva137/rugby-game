aa13da8d2b1a1b3b9d6ce1a51b15739da18c303d
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2022-03-29 01:02:22 -0300

    Add attacker and defender strategies

--- src/attacker.c
+++ src/attacker.c
@@ -9,6 +9,10 @@
 // Main header
 #include "attacker.h"
 
+// Additional headers to support random numbers generation
+#include <time.h>
+#include <stdlib.h>
+
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
@@ -16,14 +20,91 @@
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
+// Returns 0 or 1 randomly
+int generate_rand_num_0_to_1() {
+  int rand_num = rand();
+  printf("Rand: %d %d\n", rand_num, rand_num % 2);
+  return rand() / (RAND_MAX / 2 + 1);
+}
+
+// Returns 0, 1, 2, 3, 4, 5 or 6 randomly
+int generate_rand_num_0_to_6() {
+  int rand_num = rand();
+  printf("Rand6: %d e %d\n", rand_num, rand_num % 7);
+  return rand() / (RAND_MAX / 7 + 1);
+}
+
+bool is_atk_moving_up(direction_t dir) {
+  return dir.i < 0;
+}
+
+direction_t move_atk_horizontally(bool is_up_main_dir) {
+  direction_t up_mov[7] =   { DIR_RIGHT, DIR_RIGHT, DIR_RIGHT, DIR_STAY, DIR_UP_LEFT, DIR_UP_RIGHT, DIR_DOWN };
+  direction_t down_mov[7] = { DIR_RIGHT, DIR_RIGHT, DIR_RIGHT, DIR_STAY, DIR_DOWN_LEFT, DIR_DOWN_RIGHT, DIR_UP };
+
+  if (is_up_main_dir) return up_mov[generate_rand_num_0_to_6()];
+  return down_mov[generate_rand_num_0_to_6()];
+}
+
+direction_t move_atk_vertically(bool is_up_main_dir) {
+  direction_t move_up[2] = { DIR_UP, DIR_UP_RIGHT };
+  direction_t move_down[2] = { DIR_DOWN, DIR_DOWN_RIGHT };
+
+  if (is_up_main_dir) return move_up[generate_rand_num_0_to_1()];
+  return move_down[generate_rand_num_0_to_1()];
+}
+
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
-  UNUSED(defender_spy);
+  static position_t previous_position = INVALID_POSITION;
+  static direction_t best_direction = DIR_DOWN;
 
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+  direction_t vertical_mov[2] = { DIR_UP, DIR_DOWN };
+  
+  static int move_vertical = 0;
+  static bool is_up_main_dir = false;
+  if (move_vertical == 0) {
+    srand((unsigned int)time(NULL));
+  }
+
+  if (move_vertical == 0) {
+    // Choose a random vertical direction to start moving to the border
+    best_direction = vertical_mov[generate_rand_num_0_to_1()];
+    is_up_main_dir = is_atk_moving_up(best_direction);
+    move_vertical++;
+  } else {
+    if (equal_positions(attacker_position, previous_position) && move_vertical == 1) {
+      // The attacker has moved up/down and reached the border or an obstacle
+      if (get_spy_number_uses(defender_spy) == 0) {
+        // SPY the defender's position
+        position_t defender_pos = get_spy_position(defender_spy);
+
+        // If the attacker could lose by just moving to the right, change vertical direction
+        if (abs(attacker_position.i - defender_pos.i) <= 3) {
+          best_direction = is_up_main_dir ? (direction_t) DIR_DOWN : (direction_t) DIR_UP;
+          is_up_main_dir = !is_up_main_dir;
+        } else {
+          // The attacker must now start moving horizontally
+          move_vertical++;
+          best_direction = move_atk_horizontally(is_up_main_dir);
+        }
+      } else {
+        // The attacker must now start moving horizontally
+        printf("Start moving horizontally\n");
+        move_vertical++;
+        best_direction = move_atk_horizontally(is_up_main_dir);
+      }
+    } else if (move_vertical == 1) {
+      // The attacker can keep moving up/down
+      best_direction = move_atk_vertically(is_up_main_dir);
+    } else {
+      // The attacker can keep moving horizontally
+      best_direction = move_atk_horizontally(is_up_main_dir);
+    }
+  }
+
+  previous_position = attacker_position;
+  return best_direction;
 }
 
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -12,18 +12,98 @@
 // Macros
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
+// Additional headers to support random numbers generation
+#include <time.h>
+#include <stdlib.h>
+
 /*----------------------------------------------------------------------------*/
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
+// Returns 0 or 1 randomly
+int generate_random_num_0_to_1() {
+  srand(time(NULL));
+  return rand() % 2;
+}
+
+// Returns 0, 1 or 2 randomly
+int generate_random_num_0_to_2() {
+  srand(time(NULL));
+  return rand() % 3;
+}
+
+bool is_moving_up(direction_t dir) {
+  return dir.i < 0;
+}
+
+bool is_moving_left(direction_t dir) {
+  return dir.j < 0;
+}
+
+direction_t move_vertically(direction_t previous_direction, int max_up) {
+  direction_t move_up[2] = { DIR_UP, DIR_UP_LEFT };
+  direction_t move_down[2] = { DIR_DOWN, DIR_DOWN_RIGHT };
+  direction_t move_stay_down[2] = { DIR_DOWN, DIR_STAY };
+
+  if (max_up == 1) return move_stay_down[generate_random_num_0_to_1()];
+  if (is_moving_up(previous_direction) && max_up > 1) return move_up[generate_random_num_0_to_1()];
+  return move_down[generate_random_num_0_to_1()];
+}
+
 direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
-  UNUSED(attacker_spy);
+  static position_t previous_position = INVALID_POSITION;
+  static direction_t best_direction = DIR_UP;
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+  static int moved_left = false;
+
+  // Maximum number of possible movements up before hitting the border
+  int max_up = defender_position.i - 1;
+  direction_t left_mov[3] = { DIR_LEFT, DIR_UP_LEFT, DIR_DOWN_LEFT };
+
+  // Check if the defender was unable to move in the last turn
+  if (equal_positions(defender_position, previous_position)) {
+    if (!moved_left && get_spy_number_uses(attacker_spy) == 0) {
+      // If possible, SPY the attacker's position to decide which vertical direction to go
+      position_t attacker_pos = get_spy_position(attacker_spy);
+      if (attacker_pos.i >= defender_position.i) best_direction = (direction_t) DIR_DOWN_LEFT;
+      else best_direction = (direction_t) DIR_UP_LEFT;
+    } else if (!moved_left) {
+      // If not possible to SPY, keep in the same vertical direction
+      best_direction = move_vertically(best_direction, max_up);
+    } else {
+      // If the defender has already moved left, but couldn't move in the last turn, then change vertical direction
+      if (is_moving_up(best_direction)) return (direction_t) DIR_DOWN;
+      else return (direction_t) DIR_UP;
+    }
+  } else {
+    if (!moved_left && is_moving_left(best_direction)) {
+      // If the defender could move left (for the first time) in the last turn, choose a vertical direction
+      printf("Moved left!\n");
+      moved_left = true;
+
+      if (get_spy_number_uses(attacker_spy) == 0) {
+        // If possible, SPY the attacker's position to decide which vertical direction to go
+        position_t attacker_pos = get_spy_position(attacker_spy);
+        if (attacker_pos.i >= defender_position.i) best_direction = (direction_t) DIR_DOWN;
+        else best_direction = (direction_t) DIR_UP;
+      } else {
+        // If not possible to SPY, keep going in the same vertical direction (unless the defender is near the border)
+        if (is_moving_up(best_direction) && max_up > 1) best_direction = (direction_t) DIR_UP;
+        else best_direction = (direction_t) DIR_DOWN;
+      }
+    } else if (!moved_left) {
+      // If the defender hasn't moved left yet, try to move left
+      best_direction = left_mov[generate_random_num_0_to_2()];
+    } else {
+      /* If the defender has already moved left some turns before,
+         then move in the same vertical direction (unless the defender is near the border) */
+      best_direction = move_vertically(best_direction, max_up);
+    }
+  }
+
+  previous_position = defender_position;
+  return best_direction;
 }
 
 /*----------------------------------------------------------------------------*/
